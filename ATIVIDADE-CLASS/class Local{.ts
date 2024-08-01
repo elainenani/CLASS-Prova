352e3eccaa7c3c94fce53e4b06ba337e1da47e5a
elainenani <161904333+elainenani@users.noreply.github.com>
2024-07-31 21:31:02 -0400

    Add files via upload

---
+++ ATIVIDADE-CLASS/class Local{.ts
@@ -0,0 +1,33 @@
+abstract class Pessoa{
+    nome: string 
+    cpf: string 
+    idade: string
+    email: string
+    telefone: string
+    cpf_cnpj:string
+
+    construtor(nome:string, cpf:string, idade:string, email:string, telefone:string, cpf_cnpj:string){
+        this.nome = nome
+        this.nome = cpf
+        this.nome = idade
+        this.nome = email
+        this.nome = telefone
+        this.nome = cpf_cnpj
+
+        console.log("Pessoa")
+    }
+        this.lougradouro[] = []
+}  
+    console.log("cadastrar pessoa")
+
+let pessoa = new publico ("Carlos", 06967571177, 25, "carlos25@email.com")
+
+class Local{
+    rua:string
+    numero:string
+    cidade:string
+    estado:string
+ construtor(rua:string, numero:string, cidade:string){
+    
+ }
+}
